e3295431dcca4dc0d401b9db848a5929022e9102
Noora Backlund <noora.backlund@gmail.com>
2022-08-07 09:17:16 +0300

    Added part 1.11 solution

--- part1/unicafe-feedback/src/App.js
+++ part1/unicafe-feedback/src/App.js
@@ -6,7 +6,7 @@ const Button = ({ handleClick, name }) => {
   )
 }
 
-const Statistics = ({good, neutral, bad, numResponses}) => {
+const Statistics = ({ good, neutral, bad, numResponses }) => {
   if (numResponses === 0) {
     return (
       <div>
@@ -19,17 +19,21 @@ const Statistics = ({good, neutral, bad, numResponses}) => {
   return (
     <div>
       <h1>statistics</h1>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='all' value={numResponses} />
-      <StatisticLine text='average' value={(good - bad) / (numResponses)} />
-      <StatisticLine text='positive' value={(good / numResponses * 100) + ' %'} />
+      <table>
+        <tbody>
+          <StatisticLine text='good' value={good} />
+          <StatisticLine text='neutral' value={neutral} />
+          <StatisticLine text='bad' value={bad} />
+          <StatisticLine text='all' value={numResponses} />
+          <StatisticLine text='average' value={(good - bad) / (numResponses)} />
+          <StatisticLine text='positive' value={(good / numResponses * 100) + ' %'} />
+        </tbody>
+      </table>
     </div>
   )
 }
 
-const StatisticLine = ({ text, value }) => (<div>{text} {value} <br /></div>)
+const StatisticLine = ({ text, value }) => (<tr><td>{text}</td><td>{value}</td></tr>)
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -56,7 +60,7 @@ const App = () => {
       <Button name="good" handleClick={handleGood} />
       <Button name="neutral" handleClick={handleNeutral} />
       <Button name="bad" handleClick={handleBad} />
-      <Statistics good={good} bad={bad} neutral={neutral} numResponses={numResponses}/>
+      <Statistics good={good} bad={bad} neutral={neutral} numResponses={numResponses} />
     </div>
   )
 }

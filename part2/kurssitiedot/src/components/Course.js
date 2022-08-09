fb69ab9c3934d4fd2df5b0d3df890fd1387ec3eb
Noora Backlund <noora.backlund@gmail.com>
2022-08-09 07:56:49 +0300

    Added part 2.4 and 2.5 solutions

--- part2/kurssitiedot/src/components/Course.js
+++ part2/kurssitiedot/src/components/Course.js
@@ -22,6 +22,6 @@ const Content = ({parts}) => {
 
 const Part = ({name, exercises}) => <p>{name} {exercises}</p>
 
-const Total = ({parts}) => <p>total of {parts.reduce((a, b) => a + b.exercises, 0)} exercises</p>
+const Total = ({parts}) => <p><b>total of {parts.reduce((a, b) => a + b.exercises, 0)} exercises</b></p>
 
 export default Course

61d7a2fd0c796b91d02ba2e85559ab05255a1f76
Noora Backlund <noora.backlund@gmail.com>
2022-08-10 19:43:25 +0300

    Added part 2.9 solution

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -2,10 +2,19 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  // Apply filter to person list
+  const personsToShow = filter === '' 
+  ? persons
+  : persons.filter(person => person.name.toUpperCase().includes(filter.toUpperCase()))
 
   // New person addition
   const addPerson = (event) => {
@@ -27,18 +36,23 @@ const App = () => {
   }
 
   // Event handlers
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNameChange = (event) => setNewName(event.target.value)
 
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+
+  const handleFilterChange = (event) => setFilter(event.target.value)
 
   // Return page body
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with <input
+          value={filter}
+          onChange={handleFilterChange}
+        />
+      </div>
+      <h2>Add new person</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input
@@ -56,7 +70,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+        {personsToShow.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
       </div>
     </div>
   )

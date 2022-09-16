1f49f6918ebc252f4030677af3e3ebc2a31a468e
Noora Backlund <noora.backlund@gmail.com>
2022-09-16 13:17:08 +0300

    Added part 3.3 solution

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -28,10 +28,6 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello world!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
-})
-
 app.get('/info', (req, res) => {
     const response = 
         `<p>Phonebook has info for ${notes.length} people</p> \
@@ -39,7 +35,22 @@ app.get('/info', (req, res) => {
     res.send(response)
 })
 
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        res.json(note)
+    } else {
+        res.status(404).end()
+    }
+})
+
 const PORT = 3001
+
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
 })
